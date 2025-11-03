538d4dbd5cf694598f0da698f9509e1a246d3a4a
Alex Panteli <alekspanteli@gmail.com>
2025-11-03 15:24:51 +0200

    Update YourFirstComponent to display multiple Profile components with a new header for enhanced layout

--- app/your-first-component/page.tsx
+++ app/your-first-component/page.tsx
@@ -2,8 +2,13 @@ import Profile from "../components/profile";
 
 export default function YourFirstComponent() {
   return (
-    <div>
-      <Profile />
-    </div>
+    <>
+      <h1 className="mb-2">Amazing scientists</h1>
+      <div className="flex gap-4">  
+        <Profile />
+        <Profile />
+        <Profile />
+      </div>
+    </>
   );
 }
