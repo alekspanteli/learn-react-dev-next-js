8dc6c6be3c75b451d5bf2aed6144874dd630a7e2
Alex Panteli <alekspanteli@gmail.com>
2025-11-03 20:32:05 +0200

    Add Avatar2 component for user profile display and implement getImageUrl utility function; integrate Avatar2 into PassingProps component for dynamic rendering.

---
+++ app/components/avatar-2.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import { getImageUrl } from "../utils/utils";
+
+type Person = {
+  imageId: string;
+  name: string;
+};
+
+type Avatar2Props = {
+  person: Person;
+  size: 50 | 80 | 100;
+};
+
+export default function Avatar2({ person, size }: Avatar2Props) {
+  return (
+    <div>
+      <Image
+        src={getImageUrl(person)}
+        alt={person.name}
+        width={size}
+        height={size}
+      />
+    </div>
+  );
+}
--- app/passing-props/page.tsx
+++ app/passing-props/page.tsx
@@ -1,7 +1,15 @@
+import Avatar2 from "../components/avatar-2";
+
 export default function PassingProps() {
   return (
     <div className="container mx-auto p-4">
       <h1 className="mb-2">PassingProps</h1>
+      <div className="flex gap-4">
+        <Avatar2 person={{ name: 'Katsuko Saruhashi', imageId: 'OKS67lh' }} size={50} />
+        <Avatar2 person={{ name: 'Katsuko Saruhashi', imageId: 'OKS67lh' }} size={80} />
+        <Avatar2 person={{ name: 'Katsuko Saruhashi', imageId: 'OKS67lh' }} size={100} />
+      </div>
     </div>
   );
 }
+  
