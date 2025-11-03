a3137d5ebb55e65e3af719fcd71d2827982fcb08
Alex Panteli <alekspanteli@gmail.com>
2025-11-03 15:21:17 +0200

    Remove header from YourFirstComponent to streamline layout

--- app/your-first-component/page.tsx
+++ app/your-first-component/page.tsx
@@ -3,7 +3,6 @@ import Profile from "../components/profile";
 export default function YourFirstComponent() {
   return (
     <div>
-      <h1>Your First Component</h1>
       <Profile />
     </div>
   );
