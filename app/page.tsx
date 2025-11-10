b39bdb6e0ea8e1614c98a216e37fd89716abd233
Alex Panteli <alekspanteli@gmail.com>
2025-11-10 23:15:42 +0200

    Refactor Home page to introduce ShoppingList component and streamline Friend component's online status display, enhancing code clarity and user experience.

--- app/page.tsx
+++ app/page.tsx
@@ -3,37 +3,43 @@
 type FriendProps = {
   name: string;
   isOnline: boolean;
-}
+};
 
-function Friend({name, isOnline}: FriendProps) {
-  let prefix;
-
-  if (isOnline) {
-    prefix = <div className="w-2 h-2 bg-green-500 rounded-full"></div>;
-  }
-
+function Friend({ name, isOnline }: FriendProps) {
   return (
     <ul>
       <li className="flex items-center gap-2">
-        {prefix}
+        {isOnline && <div className="w-2 h-2 bg-green-500 rounded-full"></div>}
         {name}
       </li>
     </ul>
   );
 }
 
+type ShoppingListProps = {
+  items: string[];
+};
 
+const shoppinList: string[] = [];
+
+function ShoppingList({ items }: ShoppingListProps) {
+  return (
+    <ul>
+      {items.map((item) => (
+        <li key={item}>{item}</li>
+      ))}
+    </ul>
+  );
+}
 export default function Home() {
- 
-
   return (
     <>
-     <Friend name="John" isOnline={true} />
-     <Friend name="Jane" isOnline={false} />
-     <Friend name="Jim" isOnline={true} />
-     <Friend name="Jill" isOnline={false} />
-     <Friend name="Jack" isOnline={true} />
-     <Friend name="Jill" isOnline={false} />
+      <Friend name="John" isOnline={true} />
+      <Friend name="Jane" isOnline={false} />
+      
+      <br/>
+
+      {!!shoppinList.length && <ShoppingList items={shoppinList} />}
     </>
   );
 }
