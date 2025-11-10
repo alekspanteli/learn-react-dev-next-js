71cbf95b33db3e5dece7a22de6cb025cbe643ff7
Alex Panteli <alekspanteli@gmail.com>
2025-11-10 11:24:40 +0200

    Add ContactCard and Button components to Home page; update next.config.ts to allow SVG images and add new remote image pattern

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,74 @@
+"use client";
+
+type ContactCardProps = {
+  name: string;
+  job: string;
+  email: string;
+};
+
+function ContactCard({ name, job, email }: ContactCardProps) {
+  return (
+    <div className="border-2 border-gray-300 rounded-md p-4">
+      <h2 className="text-lg font-bold">{name}</h2>
+      <p className="text-sm text-gray-500">{job}</p>
+      <p className="text-sm text-gray-500">{email}</p>
+    </div>
+  );
+}
+
+type ButtonProps = {
+  color: Color;
+  children: React.ReactNode;
+  onClick: () => void;
+};
+
+const colors = {
+  red: "bg-red-500",
+  blue: "bg-blue-500",
+  green: "bg-green-500",
+} as const;
+
+type Color = keyof typeof colors;
+
+function Button({ color, children, onClick }: ButtonProps) {
+  return (
+    <button
+      className={`${colors[color]} text-white px-4 py-2 rounded-md`}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
+
 export default function Home() {
-  const email = "test@test.com";
   return (
     <>
-      <p>Email: {email.replace("@", "[at]")}</p>
-      <input type="text" placeholder="Enter your email" required={true} />
+      <ContactCard
+        name="John Doe"
+        job="Software Engineer"
+        email="john.doe@example.com"
+      />
+      <ContactCard
+        name="Jane Doe"
+        job="Software Engineer"
+        email="jane.doe@example.com"
+      />
+      <ContactCard
+        name="Jim Doe"
+        job="Software Engineer"
+        email="jim.doe@example.com"
+      />
+
+      <Button color="red" onClick={() => alert("Red button clicked")}>
+        Click me
+      </Button>
+      <Button color="blue" onClick={() => alert("Blue button clicked")}>
+        Click me
+      </Button>
+      <Button color="green" onClick={() => alert("Green button clicked")}>
+        Click me
+      </Button>
     </>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    dangerouslyAllowSVG: true,
     remotePatterns: [
       {
         protocol: "https",
         hostname: "i.imgur.com",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
       },
     ],
   },
