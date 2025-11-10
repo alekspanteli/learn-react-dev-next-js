e94438d5f7271d821e072d59a9f3b022a28b72e1
Alex Panteli <alekspanteli@gmail.com>
2025-11-10 11:50:52 +0200

    Refactor Home page to remove contacts and ContactCard components, replacing them with Badge component to display status indicators, enhancing UI clarity and functionality.

--- app/page.tsx
+++ app/page.tsx
@@ -1,89 +1,38 @@
 "use client";
 
-const contacts = [
-  {
-    name: "John Doe 1",
-    job: "Software Engineer",
-    email: "john.doe1@example.com",
-  },
-  {
-    name: "Jane Doe 2",
-    job: "Software Engineer 2",
-    email: "jane.doe2@example.com",
-  },
-  {
-    name: "Jim Doe 3",
-    job: "Software Engineer 3",
-    email: "jim.doe3@example.com",
-  },
-];
+import React from "react";
 
-type ContactCardProps = {
-  name: string;
-  job: string;
-  email: string;
-};
-
-function ContactCard({ name, job, email }: ContactCardProps) {
-  return (
-    <div className="border-2 border-gray-300 rounded-md p-4">
-      <h2 className="text-lg font-bold">{name}</h2>
-      <p className="text-sm text-gray-500">{job}</p>
-      <p className="text-sm text-gray-500">{email}</p>
-    </div>
-  );
-}
-
-type ButtonProps = {
-  color: Color;
+type BadgeProps = {
+  status: Status;
   children: React.ReactNode;
-  onClick: () => void;
 };
 
-const colors = {
-  red: "bg-red-500",
-  blue: "bg-blue-500",
-  green: "bg-green-500",
-} as const;
+const statuses = {
+  success: "bg-green-500",
+  error: "bg-red-500",
+  warning: "bg-yellow-500",
+  info: "bg-blue-500",
+};
 
-type Color = keyof typeof colors;
+type Status = keyof typeof statuses;
 
-function Button({ color, children, onClick }: ButtonProps) {
+function Badge({ status, children }: BadgeProps) {
   return (
-    <button
-      className={`${colors[color]} text-white px-4 py-2 rounded-md`}
-      onClick={onClick}
-    >
+    <span className={`px-2 py-1 rounded-md ${statuses[status]}`}>
       {children}
-    </button>
+    </span>
   );
 }
 
 export default function Home() {
+  const statusesList = Object.keys(statuses) as Status[];
   return (
     <>
-      
-      <ul className="grid gap-4">
-      {contacts
-      .filter((contact) => contact.job === "Software Engineer")
-      .map((contact, index) => (
-        <li key={index} className={`${contact.job === "Software Engineer" ? "bg-red-500" : ""}`}>
-          <ContactCard name={contact.name} job={contact.job} email={contact.email} />
-        </li>
+      {statusesList.map((status) => (
+        <React.Fragment key={status}>
+          <Badge status={status}>{status}</Badge>
+        </React.Fragment>
       ))}
-      </ul>
-
-      <br />
-
-      <Button color="red" onClick={() => alert("Red button clicked")}>
-        Click me
-      </Button>
-      <Button color="blue" onClick={() => alert("Blue button clicked")}>
-        Click me
-      </Button>
-      <Button color="green" onClick={() => alert("Green button clicked")}>
-        Click me
-      </Button>
     </>
   );
 }
