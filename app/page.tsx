970ca8a91731a696a35babfec090c960e3c71c8d
Alex Panteli <alekspanteli@gmail.com>
2025-11-11 15:10:50 +0200

    Add clickBallGame function and buttons to Home page for win/lose interaction, enhancing user engagement.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
 "use client";
 
+function clickBallGame(type: "win" | "lose") {
+   if (type === "win") {
+    console.log("You win!");
+   } else {
+    console.log("You lose!");
+   }
+}
 
 export default function Home() {
   return (
     <>
-      
+      <button onClick={() => clickBallGame("win")}>
+        Win
+      </button>
+      <button onClick={() => clickBallGame("lose")}>
+        Lose
+      </button>
     </>
   );
 }
