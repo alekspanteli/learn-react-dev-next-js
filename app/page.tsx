966feb12c3a018ec677f0300b7c0ecfa7286c3bb
Alex Panteli <alekspanteli@gmail.com>
2025-11-11 15:22:33 +0200

    Refactor Home page to remove expensiveComputation function and simplify state management, focusing on the counter functionality for improved clarity and user interaction.

--- app/page.tsx
+++ app/page.tsx
@@ -3,22 +3,11 @@
 import { useState } from "react";
 
 export default function Home() {
-  function expensiveComputation() {
-    console.log('Running expensive computation');
-    return 42;
-  }
-
-  // const [num1, setNum1] = useState(expensiveComputation());
-  const [num1, setNum1] = useState(expensiveComputation());
-
   const [count, setCount] = useState(0);
-
-
-  
 
   return (
     <>
-      {num1} {count}
+      {count}
       <button onClick={() => setCount(count + 1)}>Increment</button>
     </>
   );
