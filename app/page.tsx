afa4f994071fb1c8b2af262d41e6e0fc28c633ac
Alex Panteli <alekspanteli@gmail.com>
2025-11-11 16:10:28 +0200

    Enhance Home page form by adding acknowledgement radio buttons and improving username input handling, streamlining user interaction and data capture.

--- app/page.tsx
+++ app/page.tsx
@@ -3,21 +3,61 @@
 import { FormEvent } from "react";
 
 export default function Home() {
-
   function handleSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
     const formData = new FormData(e.target as HTMLFormElement);
-    const username = formData.get("username") as string;
-    console.log(username);
+    const entries = Object.fromEntries(formData.entries());
+    const username = entries.username?.toString().trim() ?? "";
+    const acknowledgement = entries.acknowledgement ?? null;
+
+    console.log({ username, acknowledgement });
+
     e.currentTarget.reset();
-    
   }
 
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <input className="border border-gray-300 rounded-md p-2" type="text" name="username" />
-        <button type="submit">Submit</button>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
+            className="border border-gray-300 rounded-md p-2"
+            type="text"
+            name="username"
+            id="username"
+            autoComplete="name"
+          />
+        </div>
+        <div>
+          <p>Aknowledge the terms and conditions</p>
+          <div className="flex items-center gap-2">
+            <input
+              type="radio"
+              id="yes"
+              name="acknowledgement"
+              value="yes"
+              className="accent-red-500"
+              required
+            />
+            <label htmlFor="yes">Yes</label>
+          </div>
+          <div className="flex items-center gap-2">
+            <input
+              type="radio"
+              id="no"
+              name="acknowledgement"
+              value="no"
+              className="accent-red-500"
+            />
+            <label htmlFor="no">No</label>
+          </div>
+        </div>
+        <button
+          className="bg-blue-500 text-white rounded-md p-2 cursor-pointer"
+          type="submit"
+        >
+          Submit
+        </button>
       </form>
     </>
   );
