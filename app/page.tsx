59ad1321d304c03a06293137912bc45b15830bff
Alex Panteli <alekspanteli@gmail.com>
2025-11-11 14:52:54 +0200

    Refactor Home page to remove Friend and ShoppingList components, simplifying the structure and enhancing code maintainability.

--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,11 @@
 "use client";
 
-type FriendProps = {
-  name: string;
-  isOnline: boolean;
-};
 
-function Friend({ name, isOnline }: FriendProps) {
-  return (
-    <ul>
-      <li className="flex items-center gap-2">
-        {isOnline && <div className="w-2 h-2 bg-green-500 rounded-full"></div>}
-        {name}
-      </li>
-    </ul>
-  );
-}
-
-type ShoppingListProps = {
-  items: string[];
-};
-
-const shoppinList: string[] = [];
-
-function ShoppingList({ items }: ShoppingListProps) {
-  return (
-    <ul>
-      {items.map((item) => (
-        <li key={item}>{item}</li>
-      ))}
-    </ul>
-  );
-}
 export default function Home() {
   return (
     <>
-      <Friend name="John" isOnline={true} />
-      <Friend name="Jane" isOnline={false} />
       
-      <br/>
-
-      {!!shoppinList.length && <ShoppingList items={shoppinList} />}
     </>
   );
 }
+  
