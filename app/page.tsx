6770266c3b5eab2ca42eb1634809708833034ff1
Alex Panteli <alekspanteli@gmail.com>
2025-11-12 13:00:43 +0200

    Refactor Home page to remove SearchForm and SearchResults components, replacing them with a simple input and button for username entry and focus handling, streamlining the interface and enhancing user interaction.

--- app/page.tsx
+++ app/page.tsx
@@ -1,55 +1,18 @@
 "use client";
 
-import { useState } from "react";
-
-type SearchFormProps = {
-  searchQuery: string;
-  setSearchQuery: (value: string) => void;
-};
-
-function SearchForm({ searchQuery, setSearchQuery }: SearchFormProps) {
-  function runSearch(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-  }
-  return (
-    <form onSubmit={runSearch}>
-      <input
-        className="border border-gray-300 rounded-md p-2"
-        type="text"
-        placeholder="Search"
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-      />
-      <button
-        type="submit"
-        className="bg-blue-500 text-white p-2 rounded-md cursor-pointer"
-      >
-        Search
-      </button>
-    </form>
-  );
-}
-
-function SearchResults({ searchQuery }: { searchQuery: string }) {
-  return (
-    <div>
-      <h2>Your search for: {searchQuery}.</h2>
-    </div>
-  );
-}
+import { useRef } from "react";
 
 export default function Home() {
-  const [searchQuery, setSearchQuery] = useState("");
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  function handleFocus() {
+    inputRef.current?.focus();
+  }
 
   return (
     <>
-      <header>
-        <a href="">My stuff</a>
-        <SearchForm searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
-      </header>
-      <main>
-        <SearchResults searchQuery={searchQuery} />
-      </main>
+      <input type="text" placeholder="Enter your name" ref={inputRef} />
+      <button onClick={handleFocus}>Focus Username</button>
     </>
   );
 }
